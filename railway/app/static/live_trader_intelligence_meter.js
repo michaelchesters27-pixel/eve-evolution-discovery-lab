2ce0b47544942487c7391002917a626a4ddeefc8
michaelchesters27-pixel <michaelchesters27@gmail.com>
2026-08-26 15:41:50 +0100

    Load Live Trader audit UI hardening

--- railway/app/static/live_trader_intelligence_meter.js
+++ railway/app/static/live_trader_intelligence_meter.js
@@ -11,7 +11,9 @@
       load('live_trader_safe_stops_v48.js', () =>
         load('live_trader_zone_truth_v49.js', () =>
           load('live_trader_zone_retrace_v58.js', () =>
-            load('live_trader_sections_v59.js', () => {})
+            load('live_trader_sections_v59.js', () =>
+              load('live_trader_audit_v60.js', () => {})
+            )
           )
         )
       )
