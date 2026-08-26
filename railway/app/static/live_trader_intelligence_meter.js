ed9a127d77a14b8f6803e2191bb8080e086ee340
michaelchesters27-pixel <michaelchesters27@gmail.com>
2026-08-26 15:34:00 +0100

    Load Live Trader section navigation

--- railway/app/static/live_trader_intelligence_meter.js
+++ railway/app/static/live_trader_intelligence_meter.js
@@ -10,7 +10,9 @@
     load('live_trader_execution_intelligence.js', () =>
       load('live_trader_safe_stops_v48.js', () =>
         load('live_trader_zone_truth_v49.js', () =>
-          load('live_trader_zone_retrace_v58.js', () => {})
+          load('live_trader_zone_retrace_v58.js', () =>
+            load('live_trader_sections_v59.js', () => {})
+          )
         )
       )
     )
